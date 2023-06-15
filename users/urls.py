e2f76a4a6439950cc4cd18cde73c5cd3f473a2a8
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-06-15 10:31:43 +0000

    added login view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .apiviews import CreateUserView, ListUsersView
+from .apiviews import CreateUserView, ListUsersView, LoginView
 
 urlpatterns = [
     path('', ListUsersView.as_view(), name='create-user'),
-    path('create/', CreateUserView.as_view(), name='create-user')
+    path('create/', CreateUserView.as_view(), name='create-user'),
+    path('login/', LoginView.as_view(), name='login')
 ]
