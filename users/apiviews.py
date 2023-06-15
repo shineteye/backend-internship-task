0a7b36200ab20db957dfa467fb38fa00c6238687
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-06-15 10:21:11 +0000

    added login view

--- users/apiviews.py
+++ users/apiviews.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsOwnerOrReadonly, RegularUserPermission, UserManagerPermission, AdminPermission
@@ -22,3 +25,16 @@ class UpdateUserDetails(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [UserManagerPermission, AdminPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()  # exempting the login view from permissions
+
+    def post(self, request):
+        username = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
