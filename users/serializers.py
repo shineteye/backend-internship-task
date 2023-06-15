a50dc3b316fa3576b1f57a2400c48f32d74c13f8
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-06-15 10:02:59 +0000

    overrided create method

--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'username', 'first_name', 'last_name', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
