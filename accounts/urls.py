174587e699334b41aa00616dc0cc189432c5d471
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-06-16 23:44:43 +0000

    added knox auth for login and logout + register

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from knox import views as knox_views
+from .views import LoginAPI
+from django.urls import path
+
+urlpatterns = [
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall')
+]
