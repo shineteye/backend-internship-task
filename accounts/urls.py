89323006c766c2d26823d324a8eb72b65ce22030
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-06-17 09:17:43 +0000

    clean slate again

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from knox import views as knox_views
-from .views import LoginAPI
-from django.urls import path
-
-urlpatterns = [
-    path('login/', LoginAPI.as_view(), name='login'),
-    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall')
-]
